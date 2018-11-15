92bd12cf4458a3d989251b74f1fe6a441785ab52
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-11-15 21:01:21 -0200

    done exercices

--- array/array.js
+++ array/array.js
@@ -5,3 +5,21 @@ numeros = [1,2,3,4,5]
 console.log(numeros)
 
 console.log(numeros[0])
+
+
+var arr = ['leo', null, true, 11, { bola:'azul'}, function name() {return 'oi'} ]
+
+console.log( arr[5]() )
+console.log( arr[4] )
+console.log( arr[4].bola )
+
+console.log( arr.length )
+
+
+arr.push([1,2,3])
+
+console.log( arr )
+
+console.log( arr[6] )
+console.log( arr[6][2] )
+
