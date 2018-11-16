7e34670cb8b18826065697aa7984b983accbe523
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-11-16 14:53:12 -0200

    done exercises

---
+++ typeof/typeof.js
@@ -0,0 +1,18 @@
+
+var a = 'leo';
+var b = 1;
+var c = true
+let d = null
+let e = NaN
+let f = undefined
+let g = {}
+let h = []
+
+console.log( typeof a )
+console.log( typeof b )
+console.log( typeof c )
+console.log( typeof d )
+console.log( typeof e )
+console.log( typeof f )
+console.log( typeof g )
+console.log( typeof h )
