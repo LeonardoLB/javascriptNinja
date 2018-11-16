a40378aaf944705bf7f767dca6f7044fb0676cfd
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-11-16 15:24:17 -0200

    done more exercises about typeof

--- typeof/typeof.js
+++ typeof/typeof.js
@@ -16,3 +16,15 @@ console.log( typeof e )
 console.log( typeof f )
 console.log( typeof g )
 console.log( typeof h )
+
+
+function sum(a,b) {
+    return a + b
+}
+
+console.log( sum( [], [] ) )
+console.log( sum( [], {} ) )
+console.log( sum( function (){}, [] ) )
+console.log( sum( {}, function () { } ) )
+console.log( sum( +2, -1 ) )
+console.log( sum( undefined, null ) )
