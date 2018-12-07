07c8d591b6c8e5d941d307f982f7e625c99d5bd3
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-12-07 08:26:07 -0200

    exercise: learming about ajax

--- AJAX/main.js
+++ AJAX/main.js
@@ -5,10 +5,34 @@
     var ajax = new XMLHttpRequest()
 
     // abre a requisição com o tipo e o caminho do arquivo
+    // aparece no network da pagina
     ajax.open('get', '/')
     ajax.open('get', 'main.js')
 
     //
     ajax.send()
+
+    // pode ser feito assim para escutar a mudança
+    ajax.onreadystatechange = function() {
+
+    }
+
+    // ou
+
+    ajax.addEventListener( 'readystatechange', function () {
+
+        console.log( 'terminou requisição' )
+        console.log( ajax.readyState )
+        console.log( ajax.status )
+
+    }, false )
+
+
+    // readyState,
+    // 0: não enviado, requisão não realizada (b.o no open)
+    // 1: apos abrir a conexão (conexão aberta, o executado o open)
+    // 2: headers recebidos, cabeçalhos da conexão (oq esta sendo recebido, infos sobre arquivos)
+    // 3: Carregando o conteudo/corpo do request
+    // 4: concluido com sucesso, incluiu a requisição com sucesso
 
 })(window, document)
