12ba3d0befcd7fd52d4ac43875812dd63f92e24d
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-12-24 10:32:46 -0200

    make change color

--- cssChange/main.js
+++ cssChange/main.js
@@ -5,9 +5,23 @@
     var div = document.querySelector('div')
     var counter = 0;
 
+
+
     setInterval(() => {
+
+        var colors = {
+            color1: '66FFB2',
+            color2: 'B266FF',
+            color3: 'FF007F',
+            color4: 'FF0000'
+        }
+
         counter++
-        div.style = "height: 100px; background-color: #FF"+counter+"0"+counter+"F"
+
+        if( counter > 4)
+        counter = 1;
+
+        div.style = "height: 100px; background-color: #" + colors['color'+counter] +';'
         div.style.width = '100px'
     }, 2000);
 
