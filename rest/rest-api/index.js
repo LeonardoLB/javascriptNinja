f77cb5c68abbf37a637dfeaaebd8b54ae2fee53e
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-12-25 22:17:39 -0200

    done more exercises about api rest

--- rest/rest-api/index.js
+++ rest/rest-api/index.js
@@ -4,6 +4,7 @@
 // ele busca a pasta express dentro de node_modules e incorpora o index.js desta pasta
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 // executa o arquivo atribuindo ele a uma variavel
 var app = express();
 
@@ -24,8 +25,8 @@ var pessoas = {
 
 }
 
-
 //permite o acesso a aplicação
+app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(cors());
 
 // get é o metodo que ira receber quando o metodo passado for GET
@@ -54,6 +55,14 @@ app.get( '/pessoa/:username' , function (req, res) {
     }
 
 });
+
+app.post( '/people' , function (req, res) {
+
+    var nome = req.body.username;
+    var idade = req.body.age;
+    res.json({nome: nome, idade: idade});
+
+} )
 
 
 // define a porta para acessar pelo servidor
